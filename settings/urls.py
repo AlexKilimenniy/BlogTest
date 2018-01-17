30ee4fe57d49143b27a49adc11bb11a1552cfbe8
DESKTOP-O25I9S9\alexk <alex.kilimenniy@gmail.com>
2018-01-17 12:55:43 +0300

    added urls for login, signup, logout

--- settings/urls.py
+++ settings/urls.py
@@ -17,11 +17,17 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from blog import urls as blog_urls
 from blog.api.urls import router as api_route
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
+from blog.views import UserCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'account_temp/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout_then_login, name='logout'),
+    url(r'^sign-up/$', UserCreateView.as_view(), name='signup'),
 
     # Blog
     url(r'^blog/', include(blog_urls)),
