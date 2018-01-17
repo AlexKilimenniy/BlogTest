3f8e460ed35b3a2beb4171b2bd8fa190a0f6b2bf
DESKTOP-O25I9S9\alexk <alex.kilimenniy@gmail.com>
2018-01-17 12:46:00 +0300

    added template_dir and statis_dir to settings

--- settings/urls.py
+++ settings/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from blog import urls as blog_urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # Blog
     url(r'^blog/', include(blog_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
